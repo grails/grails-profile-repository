fb7042698cfb6bf8737afc603d0fda3a0c4a4555
Graeme Rocher <graeme.rocher@gmail.com>
2014-10-07 14:41:31 +0200

    remove EnableAutoConfiguration import and annotation, as it is no longer needed

--- profiles/base/skeleton/grails-app/conf/@grails.codegen.defaultPackage.path@/Application.groovy
+++ profiles/base/skeleton/grails-app/conf/@grails.codegen.defaultPackage.path@/Application.groovy
@@ -2,9 +2,7 @@ package @grails.codegen.defaultPackage@
 
 import grails.boot.GrailsApp
 import grails.boot.config.GrailsWebConfiguration
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration
 
-@EnableAutoConfiguration
 class Application extends GrailsWebConfiguration {
     static void main(String[] args) {
         GrailsApp.run(Application)
