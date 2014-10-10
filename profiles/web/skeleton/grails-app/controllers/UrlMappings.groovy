e18dbd89408d540d1f3690e774a1996011e698ce
graemerocher <graeme.rocher@gmail.com>
2014-10-10 14:29:32 +0200

    Remove code from test application

--- profiles/web/skeleton/grails-app/controllers/UrlMappings.groovy
+++ profiles/web/skeleton/grails-app/controllers/UrlMappings.groovy
@@ -1,7 +1,6 @@
 class UrlMappings {
 
 	static mappings = {
-        "/bars"(resources:"bar")
         "/$controller/$action?/$id?(.$format)?"{
             constraints {
                 // apply constraints here
