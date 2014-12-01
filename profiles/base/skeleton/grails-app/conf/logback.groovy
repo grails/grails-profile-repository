7a190d444368638c574ea63c814e4e6c70c65010
Graeme Rocher <graeme.rocher@gmail.com>
2014-12-01 09:09:43 +0100

    configure stacktrace logger

--- profiles/base/skeleton/grails-app/conf/logback.groovy
+++ profiles/base/skeleton/grails-app/conf/logback.groovy
@@ -1,3 +1,8 @@
+import grails.util.BuildSettings
+import grails.util.Environment
+
+
+// See http://logback.qos.ch/manual/groovy.html for details on configuration
 appender('STDOUT', ConsoleAppender) {
     encoder(PatternLayoutEncoder) {
         pattern = "%level %logger - %msg%n"
@@ -5,3 +10,15 @@ appender('STDOUT', ConsoleAppender) {
 }
 
 root(ERROR, ['STDOUT'])
+
+if(Environment.current == Environment.DEVELOPMENT) {
+    appender("FULL_STACKTRACE", FileAppender) {
+        file = "${BuildSettings.TARGET_DIR}/stacktrace.log"
+        append = true
+        encoder(PatternLayoutEncoder) {
+            pattern = "%level %logger - %msg%n"
+        }
+    }
+
+    logger("StackTrace", ERROR, ['FULL_STACKTRACE'], false )
+}
