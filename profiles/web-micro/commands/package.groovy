f7d637486791c0705217af7a17106fa2a89ff332
Graeme Rocher <graeme.rocher@gmail.com>
2014-12-02 14:05:18 +0100

    fix 'package' command

--- profiles/web-micro/commands/package.groovy
+++ profiles/web-micro/commands/package.groovy
@@ -3,9 +3,23 @@ description("Packages a Grails application") {
     synonyms 'jar', 'assemble'
 }
 
-args = ['Application.groovy']
+allResources = resources("file:**/*.groovy")
+
+appName = config.getProperty('info.app.name', String) ?: baseDir.parentFile.name
+appVersion = config.getProperty('info.app.version', String) ?: '0.1'
+jar = file("${buildDir}/${appName}-${appVersion}.jar")
+args = [ jar.absolutePath ]
+args.addAll allResources.findAll { res -> !['Spec', 'Test', 'Tests'].any { res.filename.endsWith(it) }  }
+                   .sort { it.filename == "Application.groovy" ? 1 : 0 }
+                   .collect { projectPath(it.file) }
+
 args.addAll commandLine.remainingArgs
-return spring.jar(*args) == 0 ? true : false
+if(spring.jar(*args) == 0) {
+    addStatus "Built JAR file ${projectPath(jar)}"
+}
+else {
+    error "Error building JAR file"
+}
 
 
 
