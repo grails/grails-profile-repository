7c89e5811d75db3adf4e88f05eb18acd7f00ed4e
Lari Hotari <lhotari@pivotal.io>
2015-02-10 23:13:56 -0500

    GRAILS-11961 handle running integration tests in test-app

--- profiles/base/commands/test-app.groovy
+++ profiles/base/commands/test-app.groovy
@@ -4,10 +4,9 @@ description("Runs the applications tests") {
     synonyms 'test'
     argument name:"Test Name", description:"The name of the test to run (optional)", required:false
     flag name:'debug-jvm'
+    flag name:'unit', description:"Run unit tests (test target)"
+    flag name:'integration', description:"Run integration tests (integrationTest target)"
 }
-
-
-arguments = []
 
 // configure environment to test is not specified
 if(!commandLine.isEnvironmentSet()) {
@@ -15,16 +14,37 @@ if(!commandLine.isEnvironmentSet()) {
 }
 
 // add test.single argument if specified
-args = commandLine.remainingArgs.collect { "--tests $it".toString() }.join(' ')
+def testsFilter = commandLine.remainingArgs.collect { "--tests $it".toString() }.join(' ')?.trim()
+boolean debugJvm = flag('debug-jvm')
 
-// add debug flag if present
+def handleTestPhase = { targetName ->
+    def args = []
+    args << targetName
+    if(testsFilter) {
+        args << testsFilter
+    }
+    // add debug flag if present
+    if(debugJvm) {
+        args << "--debug-jvm"
+    }
+    args
+}
+
+def gradleArgs = []
+
+boolean executeUnitTests = flag('unit') || !flag('integration')
+if(executeUnitTests) {
+    gradleArgs += handleTestPhase('test')
+} 
+
+boolean hasIntegrationTests = file("src/integration-test").isDirectory()
+boolean executeIntegrationTests = hasIntegrationTests && (flag('integration') || !flag('unit'))
+if(executeIntegrationTests) {
+    gradleArgs += handleTestPhase('integrationTest')
+}
+
 try {
-    if(flag('debug-jvm')) {
-        gradle."test $args --debug-jvm"(*arguments)
-    }
-    else {
-        gradle."test $args"(*arguments)
-    }
+    gradle."${gradleArgs.join(' ')}"()
     addStatus "Tests PASSED"
     return true
 } catch(e) {
