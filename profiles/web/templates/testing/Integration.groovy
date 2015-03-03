02f78f7bf17d5899c522bbf4eebeb67bdfdebe25
graemerocher <graeme.rocher@gmail.com>
2015-03-03 14:31:11 +0100

    rollback changes in each test method by default

--- profiles/web/templates/testing/Integration.groovy
+++ profiles/web/templates/testing/Integration.groovy
@@ -1,14 +1,14 @@
 @artifact.package@
 
 import grails.test.mixin.integration.Integration
-import grails.transaction.Transactional
-
+import grails.transaction.*
 import spock.lang.*
 
 /**
  * See the API for {@link grails.test.mixin.support.GrailsUnitTestMixin} for usage instructions
  */
 @Integration
+@Rollback
 class @artifact.name@Spec extends Specification {
 
     def setup() {
