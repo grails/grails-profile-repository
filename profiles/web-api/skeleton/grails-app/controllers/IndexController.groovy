34a30d0203e22464a1c5002564f558bcf99210de
karaznie <karaznie@gmail.com>
2015-04-02 20:34:53 +0200

    Grails web API profile, code cleanup

--- profiles/web-api/skeleton/grails-app/controllers/IndexController.groovy
+++ profiles/web-api/skeleton/grails-app/controllers/IndexController.groovy
@@ -3,27 +3,27 @@ import grails.core.GrailsApplication
 
 class IndexController {
 
-  def GrailsApplication grailsApplication
-  
-  def index() {
-    render(contentType: 'application/json') {
-      message = "Welcome to Grails!"
-      environment = grails.util.Environment.current.name
-      appversion = grailsApplication.metadata['info.app.version']
-      grailsversion = grailsApplication.metadata['info.app.grailsVersion']
-      appprofile = grailsApplication.config.grails?.profile
-      groovyversion = GroovySystem.getVersion()
-      jvmversion = System.getProperty('java.version')
-      controllers = array {
-        for (c in grailsApplication.controllerClasses)
-        controller([name: c.fullName])
-      }
-      plugins = array {
-        for (p in grailsApplication.mainContext.getBean('pluginManager').allPlugins)
-        plugin([name: p.fullName])
-      }
-    }
+    def GrailsApplication grailsApplication
 
-  }
+    def index() {
+        render(contentType: 'application/json') {
+            message = "Welcome to Grails!"
+            environment = grails.util.Environment.current.name
+            appversion = grailsApplication.metadata['info.app.version']
+            grailsversion = grailsApplication.metadata['info.app.grailsVersion']
+            appprofile = grailsApplication.config.grails?.profile
+            groovyversion = GroovySystem.getVersion()
+            jvmversion = System.getProperty('java.version')
+            controllers = array {
+                for (c in grailsApplication.controllerClasses)
+                    controller([name: c.fullName])
+            }
+            plugins = array {
+                for (p in grailsApplication.mainContext.getBean('pluginManager').allPlugins)
+                    plugin([name: p.fullName])
+            }
+        }
+
+    }
 
 }
--- profiles/web-api/skeleton/grails-app/controllers/NotFoundController.groovy
+++ profiles/web-api/skeleton/grails-app/controllers/NotFoundController.groovy
@@ -1,10 +1,10 @@
-
 class NotFoundController {
 
     def index() {
-      render(contentType: 'application/json') {
-        error = 404
-        message = "Not Found"
-      }
+        render(contentType: 'application/json') {
+            error = 404
+            message = "Not Found"
+        }
     }
+
 }
