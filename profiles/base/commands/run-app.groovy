be669a5854d65ee6518a2f7e41cce09f4dea6438
Graeme Rocher <graeme.rocher@gmail.com>
2015-06-05 08:40:31 +0200

    Better handling of SSL config

--- profiles/base/commands/run-app.groovy
+++ profiles/base/commands/run-app.groovy
@@ -29,15 +29,28 @@ try {
         if(!System.getProperty('grails.server.ssl.key-store')) {
             def keystoreDir = new File(buildDir, "ssl")
             def keystoreFile = new File(keystoreDir, 'keystore')
+
+            boolean keyStoreParametersAvailable = false
             if(!keystoreFile.exists()) {
                 console.updateStatus "Generating SSL certificate"
-                createSSLCertificate(keystoreDir)
-                console.updateStatus "Generated SSL certificate"
+                if ( createSSLCertificate(keystoreDir) ) {
+                    keyStoreParametersAvailable = true
+                    console.updateStatus "Generated SSL certificate"
+                }
+                else {
+                    console.warn "Unable to automatically generate SSL certificate, manual configuration required. Set 'server.ssl.key-store' in application.yml"   
+                }
+                
             }
-            arguments << "-Dgrails.server.ssl.key-store=${keystoreFile}"
-            arguments << "-Dgrails.server.ssl.key-store-password=123456"
-            arguments << "-Dgrails.server.ssl.key-password=123456"
+            else {
+                keyStoreParametersAvailable = true
+            }
+            if(keyStoreParametersAvailable) {
+                arguments << "-Dgrails.server.ssl.key-store=${keystoreFile}"
+                arguments << "-Dgrails.server.ssl.key-store-password=123456"
+                arguments << "-Dgrails.server.ssl.key-password=123456"
 
+            }
         }
     }
 
@@ -58,32 +71,51 @@ catch(Throwable e) {
 }
 
 
-protected createSSLCertificate(File keystoreDir) {
+protected boolean createSSLCertificate(File keystoreDir) {
 
     if (!keystoreDir.exists() && !keystoreDir.mkdir()) {
         throw new RuntimeException("Unable to create keystore folder: $keystoreDir.canonicalPath")
     }
 
     def keyToolClass = getKeyToolClass()
-    def keystoreFile = new File(keystoreDir, "keystore")
-    keyToolClass.main(
-        "-genkey",
-        "-alias", "localhost",
-        "-dname", "CN=localhost,OU=Test,O=Test,C=US",
-        "-keyalg", "RSA",
-        "-validity", "365",
-        "-storepass", "key",
-        "-keystore", keystoreFile.absolutePath,
-        "-storepass", "123456",
-        "-keypass", "123456")
+    if(keyToolClass) {
+        try {
+            def keystoreFile = new File(keystoreDir, "keystore")
+            keyToolClass.main(
+                "-genkey",
+                "-alias", "localhost",
+                "-dname", "CN=localhost,OU=Test,O=Test,C=US",
+                "-keyalg", "RSA",
+                "-validity", "365",
+                "-storepass", "key",
+                "-keystore", keystoreFile.absolutePath,
+                "-storepass", "123456",
+                "-keypass", "123456")
+        }
+        catch(Throwable e) {
+            return false
+        }
+        
+        return true
+    }
+    else {
+        return false
+    }
 }
 
-protected getKeyToolClass() {
+protected Class getKeyToolClass() {
     try {
-        Class.forName 'sun.security.tools.KeyTool'
+        try {
+            return Class.forName( 'sun.security.tools.KeyTool' )
+        }
+        catch (ClassNotFoundException e) {
+            // no try/catch for this one, if neither is found let it fail
+            return Class.forName( 'com.ibm.crypto.tools.KeyTool' )
+        }
+        
     }
-    catch (ClassNotFoundException e) {
-        // no try/catch for this one, if neither is found let it fail
-        Class.forName 'com.ibm.crypto.tools.KeyTool'
+    catch(Throwable e) {
+        return null
     }
+    
 }
