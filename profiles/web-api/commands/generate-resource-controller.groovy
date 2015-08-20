c4f1be953356ac266c60a12c41cbac0ed4ed0621
Graeme Rocher <graeme.rocher@gmail.com>
2015-08-20 15:16:03 +0200

    add command to generate a resource controller

---
+++ profiles/web-api/commands/generate-resource-controller.groovy
@@ -0,0 +1,42 @@
+import org.grails.cli.interactive.completers.DomainClassCompleter
+
+description( "Generates a controller that performs REST operations" ) {
+  usage "grails generate-resource-controller [DOMAIN CLASS]"
+  completer DomainClassCompleter
+  flag name:'force', description:"Whether to overwrite existing files"
+}
+
+
+if(args) {
+  def classNames = args
+  if(args[0] == '*') {
+    classNames = resources("file:grails-app/domain/**/*.groovy")
+                    .collect { className(it) }
+  }
+  for(arg in classNames) {
+    def sourceClass = source(arg)
+    def overwrite = flag('force') ? true : false
+    if(sourceClass) {
+      def model = model(sourceClass)
+      render template: template('artifacts/scaffolding/Controller.groovy'), 
+             destination: file("grails-app/controllers/${model.packagePath}/${model.convention('Controller')}.groovy"),
+             model: model,
+             overwrite: overwrite
+
+      // TODO: Produce a test
+      // render template: template('scaffolding/Spec.groovy'), 
+      //        destination: file("src/test/groovy/${model.packagePath}/${model.convention('ControllerSpec')}.groovy"),
+      //        model: model,
+      //        overwrite: overwrite
+
+
+      addStatus "Scaffolding completed for ${projectPath(sourceClass)}"                                         
+    }
+    else {
+      error "Domain class not found for name $arg"
+    }
+  }
+}
+else {
+    error "No domain class specified"
+}
---
+++ profiles/web-api/templates/artifacts/scaffolding/Controller.groovy
@@ -0,0 +1,81 @@
+<%=packageName ? "package ${packageName}" : ''%>
+
+import static org.springframework.http.HttpStatus.*
+import grails.transaction.Transactional
+
+@Transactional(readOnly = true)
+class ${className}Controller {
+
+    static responseFormats = ['json', 'xml']
+    static allowedMethods = [save: "POST", update: "PUT", delete: "DELETE"]
+
+    def index(Integer max) {
+        params.max = Math.min(max ?: 10, 100)
+        respond ${className}.list(params), model:[${propertyName}Count: ${className}.count()]
+    }
+
+    def show(${className} ${propertyName}) {
+        respond ${propertyName}
+    }
+
+    def create() {
+        respond new ${className}(params)
+    }
+
+    @Transactional
+    def save(${className} ${propertyName}) {
+        if (${propertyName} == null) {
+            transactionStatus.setRollbackOnly()
+            render status: NOT_FOUND
+            return
+        }
+
+        if (${propertyName}.hasErrors()) {
+            transactionStatus.setRollbackOnly()
+            respond ${propertyName}.errors, view:'create'
+            return
+        }
+
+        ${propertyName}.save flush:true
+
+        respond ${propertyName}, [status: CREATED]
+    }
+
+    def edit(${className} ${propertyName}) {
+        respond ${propertyName}
+    }
+
+    @Transactional
+    def update(${className} ${propertyName}) {
+        if (${propertyName} == null) {
+            transactionStatus.setRollbackOnly()
+            render status: NOT_FOUND
+            return
+        }
+
+        if (${propertyName}.hasErrors()) {
+            transactionStatus.setRollbackOnly()
+            respond ${propertyName}.errors, view:'edit'
+            return
+        }
+
+        ${propertyName}.save flush:true
+
+        respond ${propertyName}, [status: OK] 
+    }
+
+    @Transactional
+    def delete(${className} ${propertyName}) {
+
+        if (${propertyName} == null) {
+            transactionStatus.setRollbackOnly()
+            render status: NOT_FOUND
+            return
+        }
+
+        ${propertyName}.delete flush:true
+
+        render status: NO_CONTENT 
+    }
+
+}
