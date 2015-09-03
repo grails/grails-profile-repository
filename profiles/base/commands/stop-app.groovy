ad9c24326eb5af18b227b52b010fefe365f1333f
Graeme Rocher <graeme.rocher@gmail.com>
2015-09-03 17:20:15 +0200

    shutdown via JMX

--- profiles/base/commands/stop-app.groovy
+++ profiles/base/commands/stop-app.groovy
@@ -1,3 +1,8 @@
+import javax.management.remote.JMXServiceURL
+import javax.management.remote.JMXConnectorFactory
+import javax.management.ObjectName
+import org.grails.io.support.*
+
 description("Stops the running Grails application") {
     usage "grails stop-app"
     synonyms 'stop'
@@ -5,17 +10,73 @@ description("Stops the running Grails application") {
     flag name:'host', description:"Specifies the host the Grails application is bound to"
 }
 
-Integer port = flag('port')?.toInteger() ?: config.getProperty('server.port', Integer) ?: 8080
-String host = flag('host') ?: config.getProperty('server.address', String) ?: "localhost"
-String path = config.getProperty('server.context-path') ?: config.getProperty('server.contextPath') ?: ""
-console.updateStatus "Shutting down application..."
-def url = new URL("http://$host:${port}${path}/shutdown")
-def connection = url.openConnection()
-connection.setRequestMethod("POST")
-connection.doOutput = true
-connection.connect()
-console.updateStatus connection.content.text
-while(isServerAvailable(host, port)) {
-	sleep 100
+def getJMXLocalConnectorAddresses = { applicationMainClassName ->
+    final String CONNECTOR_ADDRESS = "com.sun.management.jmxremote.localConnectorAddress"
+    def VirtualMachine = getClass().classLoader.loadClass('com.sun.tools.attach.VirtualMachine')
+    return VirtualMachine.list()
+        .findAll { 
+        	it.displayName() == applicationMainClassName 
+        }
+        .collect { desc ->
+            def vm = VirtualMachine.attach(desc.id())
+            try {
+                def connectorAddress = vm.agentProperties.getProperty(CONNECTOR_ADDRESS)
+                if (connectorAddress == null) {
+                    // Trying to load agent
+                    def agent = [vm.systemProperties.getProperty("java.home"), "lib", "management-agent.jar"].join(File.separator)
+                    vm.loadAgent(agent)
+
+                    connectorAddress = vm.agentProperties.getProperty(CONNECTOR_ADDRESS)
+                }
+                if (connectorAddress) {
+                    return connectorAddress
+                }
+            } finally {
+                vm.detach()
+            }
+        }.findAll { it }
 }
 console.updateStatus "Application shutdown."
+final applicationMainClassName = MainClassFinder.findMainClass()
+def addresses = applicationMainClassName ? getJMXLocalConnectorAddresses(applicationMainClassName) : null
+if(addresses) {
+	JMXServiceURL url = new JMXServiceURL(addresses[0])
+	def connector = JMXConnectorFactory.connect(url)
+
+	try {
+	    def server = connector.MBeanServerConnection
+
+	    def objectName = new ObjectName("org.springframework.boot:type=Endpoint,name=shutdownEndpoint")
+	    def modules = server.queryNames(objectName, null).collect { new GroovyMBean(server, it) }
+	    if(modules) {
+		    modules.each { mbean ->
+		    	console.addStatus "Shutting down application..."
+		        mbean.shutdown()
+		        console.addStatus "Application shutdown."
+		        return true
+		    }
+	    }
+
+	} finally {
+	    connector.close()
+	}	
+}
+else {
+	console.addStatus "Application not found via JMX, attempting remote shutdown."
+	Integer port = flag('port')?.toInteger() ?: config.getProperty('server.port', Integer) ?: 8080
+	String host = flag('host') ?: config.getProperty('server.address', String) ?: "localhost"
+	String path = config.getProperty('server.context-path') ?: config.getProperty('server.contextPath') ?: ""
+	console.updateStatus "Shutting down application..."
+	def url = new URL("http://$host:${port}${path}/shutdown")
+	def connection = url.openConnection()
+	connection.setRequestMethod("POST")
+	connection.doOutput = true
+	connection.connect()
+	console.updateStatus connection.content.text
+	while(isServerAvailable(host, port)) {
+		sleep 100
+	}
+	console.updateStatus "Application shutdown."
+	return true
+}
+
