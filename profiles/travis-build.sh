ba96630bc00fafb4442e2dde4c152b78506b0e33
graemerocher <graeme.rocher@gmail.com>
2015-10-01 17:45:39 +0200

    Include Travis build

---
+++ profiles/travis-build.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+set -e
+./gradlew clean 
+EXIT_STATUS=0
+echo "Publishing archives for branch $TRAVIS_BRANCH"
+if [[ -n $TRAVIS_TAG ]] || [[ $TRAVIS_BRANCH == 'master' && $TRAVIS_PULL_REQUEST == 'false' ]]; then
+
+  echo "Publishing archives"
+
+  if [[ -n $TRAVIS_TAG ]]; then
+      ./gradlew bintrayUpload || EXIT_STATUS=$?
+  else
+      ./gradlew publish || EXIT_STATUS=$?
+  fi
+
+fi
