a9bcbe8ff781632a061daba1ad52d0051a437656
Craig Burke <craig@craigburke.com>
2015-12-20 09:35:51 -0500

    Add separate template for NgDomainFactory and NgDomain fallback template.

--- profiles/angular/skeleton/grails-app/assets/javascripts/@grails.codegen.defaultPackage.path@/core/services/DomainServiceFactory.js
+++ profiles/angular/skeleton/grails-app/assets/javascripts/@grails.codegen.defaultPackage.path@/core/services/DomainServiceFactory.js
@@ -1,15 +1,21 @@
 //= wrapped
 
+/*
+    NOTE: This file is used by the create-ng-domain action.
+    You can modify or extend the DomainServiceFactory but it is recommended that you not delete it.
+*/
+
 angular
     .module("@grails.codegen.defaultPackage@.core")
     .factory("DomainServiceFactory", DomainServiceFactory);
 
 function DomainServiceFactory($resource) {
-    return function(url) {
+    return function(url, paramDefaults, actions, options) {
         return $resource(
-            url + "/:id",
-            null,
-            {"update": {method: "PUT"}, "list": {method: "GET", isArray: true}}
+            url,
+            paramDefaults || null,
+            actions || {"update": {method: "PUT"}, "list": {method: "GET", isArray: true}},
+            options || {}
         );
     }
 }
--- profiles/angular/templates/NgDomain.js
+++ profiles/angular/templates/NgDomain.js
@@ -4,6 +4,11 @@ angular
     .module("${moduleName}")
     .factory("${className}", ${className});
 
-function ${className}(DomainServiceFactory) {
-    return DomainServiceFactory("${propertyName}");
+function ${className}(\$resource) {
+    var ${className} = \$resource(
+        "${propertyName}/:id",
+        null,
+        {"update": {method: "PUT"}, "list": {method: "GET", isArray: true}}
+    );
+    return ${className};
 }
