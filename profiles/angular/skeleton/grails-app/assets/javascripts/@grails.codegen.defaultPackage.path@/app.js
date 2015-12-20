1f3bcb1de215cec3ff98653ecebbc12305c08538
Craig Burke <craig@craigburke.com>
2015-12-20 14:07:52 -0500

    Removing unneeded asset references from main module

--- profiles/angular/skeleton/grails-app/assets/javascripts/@grails.codegen.defaultPackage.path@/app.js
+++ profiles/angular/skeleton/grails-app/assets/javascripts/@grails.codegen.defaultPackage.path@/app.js
@@ -2,12 +2,6 @@
 //= require /angular/angular
 //= require /@grails.codegen.defaultPackage.path@/core/core
 //= require /@grails.codegen.defaultPackage.path@/index/index
-//= require_self
-//= require_tree services
-//= require_tree controllers
-//= require_tree directives
-//= require_tree domain
-//= require_tree templates
 
 angular.module("@grails.codegen.defaultPackage@", [
     "@grails.codegen.defaultPackage@.core",
