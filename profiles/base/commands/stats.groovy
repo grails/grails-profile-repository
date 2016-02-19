98f7e0f118c72910a81d5ba08fb5011bc12f6d40
ejaz <ejaz_ahmed@outlook.com>
2016-02-19 17:28:34 +0500

    Adding URL Mappings stats

--- profiles/base/commands/stats.groovy
+++ profiles/base/commands/stats.groovy
@@ -27,6 +27,7 @@ def DEFAULT_LOC_MATCHER = { file ->
 // maps file path to
 def pathToInfo = [
     [name: "Controllers",        path: "^grails-app.controllers",      filetype: ["Controller.groovy"]],
+    [name: "URL Mappings",       path: "^grails-app.controllers",      filetype: ["UrlMappings.groovy"]],
     [name: "Interceptors",       path: "^grails-app.controllers",      filetype: ["Interceptor.groovy"]],
     [name: "Domain Classes",     path: "^grails-app.domain",           filetype: [".groovy"]],
     [name: "Jobs",               path: "^grails-app.job",              filetype: [".groovy"]],
