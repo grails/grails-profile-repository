48905c037dbf2201b768127216c8089189c2619f
James Kleeh <james.kleeh@gmail.com>
2016-05-13 13:41:05 -0400

    Fix typos. Add ui-router dependency to index

--- profiles/angular/skeleton/grails-app/assets/javascripts/@grails.codegen.defaultPackage.path@/index/@grails.codegen.defaultPackage@.index.js
+++ profiles/angular/skeleton/grails-app/assets/javascripts/@grails.codegen.defaultPackage.path@/index/@grails.codegen.defaultPackage@.index.js
@@ -5,9 +5,23 @@
 //= require_tree directives
 //= require_tree templates
 //= require /angular/ui-bootstrap-tpls
+//= require /angular/angular-ui-router
 
-angular.module("@grails.codegen.defaultPackage@.index", [
-  "@grails.codegen.defaultPackage@.core",
-  "ui.bootstrap.dropdown",
-  "ui.bootstrap.collapse",
-]);
+
+    angular.module("@grails.codegen.defaultPackage@.index", [
+        "@grails.codegen.defaultPackage@.core",
+        "ui.bootstrap.dropdown",
+        "ui.bootstrap.collapse",
+        "ui.router"
+    ])
+    .config(config);
+
+function config($stateProvider, $urlRouterProvider) {
+    $stateProvider
+        .state('index', {
+            url: "/",
+            templateUrl: "/@grails.codegen.defaultPackage.path@/index/index.html"
+        });
+
+    $urlRouterProvider.otherwise('/');
+}
