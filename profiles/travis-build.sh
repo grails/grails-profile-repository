75109ab0ea3334a5793d718cb9eab3dc4810e55c
James Kleeh <james.kleeh@gmail.com>
2016-08-01 22:01:26 -0400

    Update travis-build.sh
    No longer publish from this repository

--- profiles/travis-build.sh
+++ profiles/travis-build.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 set -e
-cd profiles
-./gradlew clean 
+#cd profiles
+#./gradlew clean 
 EXIT_STATUS=0
-echo "Publishing archives for branch $TRAVIS_BRANCH"
-if [[ -n $TRAVIS_TAG ]] || [[ $TRAVIS_BRANCH == '3.1.x' && $TRAVIS_PULL_REQUEST == 'false' ]]; then
+#echo "Publishing archives for branch $TRAVIS_BRANCH"
+#if [[ -n $TRAVIS_TAG ]] || [[ $TRAVIS_BRANCH == '3.1.x' && $TRAVIS_PULL_REQUEST == 'false' ]]; then
 
-  echo "Publishing archives"
+#  echo "Publishing archives"
 
-  if [[ -n $TRAVIS_TAG ]]; then
-      ./gradlew bintrayUpload || EXIT_STATUS=$?
-  else
-      ./gradlew publish || EXIT_STATUS=$?
-  fi
+#  if [[ -n $TRAVIS_TAG ]]; then
+#      ./gradlew bintrayUpload || EXIT_STATUS=$?
+#  else
+#      ./gradlew publish || EXIT_STATUS=$?
+#  fi
 
-fi
+#fi
 
 exit $EXIT_STATUS
